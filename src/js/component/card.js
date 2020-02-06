6df5b03f1039da3e472f1f88744e7ebf7243ea57
yusbel86 <yusbel1086@gmail.com>
2020-02-06 02:02:40 +0000

    Added jumbo

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -4,14 +4,14 @@ import PropTypes from "prop-types";
 //create your first component
 export function Card(props) {
 	return (
-		<div className="card text-center" style={{ width: "280px" }}>
+		<div className="card text-center mx-5" style={{ width: "280px" }}>
 			<div className="card-body">
 				<h5 className="card-title">{props.title}</h5>
 				<p className="card-text">
 					With supporting text below as a natural lead-in to
 					additional content.
 				</p>
-				<a href={props.Url} className={props.bootons}>
+				<a href={props.Url} className={props.button}>
 					Go somewhere
 				</a>
 			</div>
@@ -22,5 +22,5 @@ export function Card(props) {
 Card.propTypes = {
 	Url: PropTypes.string,
 	title: PropTypes.string,
-	bootons: PropTypes
+	button: PropTypes.string
 };
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -4,16 +4,32 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 import { Navbar } from "./navbar";
+import { Jumbo } from "./jumbo";
 import { Card } from "./card";
 //create your first component
 export function Home() {
 	return (
 		<div className="text-center mt-5">
 			<Navbar />
-			<div className="d-flex justify-content-start">
-				<Card Url="http://www.marca.com" title="Marca" />
-				<Card Url="http://www.google.com" title="Google" />
-				<Card />
+
+			<Jumbo />
+
+			<div className="d-flex justify-content-center">
+				<Card
+					Url="http://www.marca.com"
+					title="Marca"
+					button="btn btn-primary"
+				/>
+				<Card
+					Url="http://www.google.com"
+					title="Google"
+					button="btn btn-success"
+				/>
+				<Card
+					Url="http://www.cope.es"
+					title="La Cope"
+					button="btn btn-info"
+				/>
 			</div>
 		</div>
 	);
